246bdffff96d294329e7f7310155372872ad706b
Bjorn Omholt <bjorn.omholt@revolve.no>
2016-06-26 03:24:23 +0200

    af

--- TelemetryDataUnpacking.py
+++ TelemetryDataUnpacking.py
@@ -680,10 +680,11 @@
 ],
 
 '35A': lambda data: [
-	('ECU_TPS_R'	 , hex_to_uint16(get_byte(data, 0) + get_byte(data, 1)) / 10.0 ),
-	('ECU_TPS_L'	 , hex_to_uint16(get_byte(data, 2) + get_byte(data, 3)) / 10.0 )
+	('ECU_TPS_L'	 , hex_to_uint16(get_byte(data, 0) + get_byte(data, 1)) / 10.0 )
 ],
-
+'35B': lambda data: [
+	('ECU_TPS_R'	 , hex_to_uint16(get_byte(data, 0) + get_byte(data, 1)) / 10.0 )
+],
 '450': lambda data: [
 	('ECU_STATUS'	 , hex_to_uint32(get_byte(data, 0) + get_byte(data, 1) + get_byte(data, 2) +get_byte(data, 3)) )
 ],
